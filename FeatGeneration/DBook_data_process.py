51640b1ac5de97dd37f98a783666b776a105981c
SihaoHu <husihao26@gmail.com>
2021-11-19 09:27:58 +0800

    DBook item/user feat

--- FeatGeneration/DBook_data_process.py
+++ FeatGeneration/DBook_data_process.py
@@ -20,7 +20,7 @@ ui_data = pd.read_csv(input_dir+'user_book.txt', names=['user','item','rating'],
 ui_data.loc[ui_data[ui_data.rating < 4].index, 'label'] = "0"
 ui_data.loc[ui_data[ui_data.rating >= 4].index, 'label'] = "1"
 ui_data = ui_data.astype("str")
-
+pos_ui_data = ui_data[ui_data.label==1].copy()
 
 # read item/user feature
 ul = pd.read_csv(input_dir+'user_location.txt', names=['user','location'], sep='\t',engine='python')
@@ -47,4 +47,9 @@ def udf(df):
     return np.array([[df.iloc[0]["user"], "".join(df.group.values.astype(str))]])
 
 ug_ = ug.groupby(["user"]).apply(udf)
+ugs = pd.DataFrame(np.concatenate(ug_.values,axis=0),columns=["user","groups"])
 
+user_feat_df = pd.merge(left=ugs, right=ul, on="user", how="inner")
+item_feat_df = pd.merge(left=pd.merge(left=bp, right=ba, how="inner"), right=by, on="user", how="inner")
+
+# training_sample 不构建复杂的sequence特征, 因为没有timestamp信息

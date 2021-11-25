b51dfdd83151d84a1dab880e9bbb3aa4db40b33d
SihaoHu <husihao26@gmail.com>
2021-11-25 09:33:37 +0800

    test2

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,6 +1,6 @@
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
 
 def res_layer(inp, dim, name):
 
